7a56252eb3ef09fce5dd282f609c562e1a10c25f
Yuanjun Zhong <zhuimengzyj@gmail.com>
2019-08-13 22:01:40 +0800

    feat: complete ch12-3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::{env, process};
+use minigrep::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    // - Why can it pass Vec<String>?
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing argument: {}", err);
+        process::exit(1);
+    });
+
+    println!("search for {}", config.query);
+    println!("in file {}", config.filename);
+    if let Err(e) = minigrep::run(config) {
+        println!("error: {}", e);
+        process::exit(1);
+    }
 }
