9ea579e6eb797ffd8ac7099a98244a9c30abc4af
Yuanjun Zhong <zhuimengzyj@gmail.com>
2019-08-13 22:35:25 +0800

    feat: complete ch12-4

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
-use std::{env, fs, process};
 use std::error::Error;
+use std::fs;
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+pub fn run(config: Config) -> Result<(), Box<Error>> {
     let contents = fs::read_to_string(&config.filename)?;
-    println!("With text: \n{}", contents);
+    let results = search(&config.query, &contents);
+    println!("result is:");
+    for line in results.iter() {
+        println!("{}", line)
+    }
     Ok(())
 }
 
@@ -19,11 +23,32 @@ impl Config {
         }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Ok(
-            Config {
-                query,
-                filename,
-            }
-        )
+        Ok(Config { query, filename })
+    }
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let content = "
+Rust:
+safe, fast, productive.
+Pick three.
+        ";
+        assert_eq!(vec!["safe, fast, productive."], search(query, content))
     }
 }
